410e2d6300c3c68f17b330bd33bcb2c4060dd052
ampandey <amritpandey2005@gmail.com>
2026-06-30 19:03:55 +0530

    creating first API using FastAPI

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+posts: list[dict]= [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "FastAPI is Awesome",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+]
+
+@app.get("/", response_class = HTMLResponse, )
+def home():
+    return f"<h1>{posts[0]['title']}</h>?"
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
