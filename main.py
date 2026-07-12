b4446f82f683cf680f773c853007a016e93f196d
ampandey <amritpandey2005@gmail.com>
2026-07-12 09:54:37 +0530

    creating routes and handle errors

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -10,21 +13,47 @@ posts: list[dict]= [
     "id": 1,
     "title": "FastAPI is Awesome",
     "content": "FastAPI is a great framework for python, building highly and scalable web APIs.",
-    "author": "Corey Schafer"
+    "author": "Corey Schafer",
+    "date_posted": "11 July 2026"
   },
   {
     "id": 2,
     "title": "Python is a Great programming language for biginners",
     "content": "Python is a Great programming language for biginners, and FastAPI makes it even better.",
-    "author": "Amrit Pandey"
+    "author": "Amrit Pandey",
+    "date_posted": "12 July 2026"
   },
 ]
+# Frontend Navigation Routes
 
-@app.get("/", include_in_schema=False)
-@app.get("/posts", include_in_schema=False)
+@app.get("/", include_in_schema=False, name='home')
+@app.get("/posts", include_in_schema=False, name='posts')
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
 
-@app.get("/api/posts")
+@app.get('/posts/{post_id}')
+def post_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return templates.TemplateResponse(request, "post.html", {"post": post, "title": "Post"})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!")
+
+@app.get('/posts/{post_id}')
+def error_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return templates.TemplateResponse(request, "error.html")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!")
+
+# API ROUTES
+
+@app.get('/api/posts')
 def get_posts():
     return posts
+
+@app.get('/api/posts/{post_id}')
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!")
